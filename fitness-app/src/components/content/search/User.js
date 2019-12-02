e90d40af254979f40c765dae412ccda42efef0b6
wesenbergg <b.b.j.1998@gmail.com>
2019-12-02 17:53:45 +0200

    modified search page

--- fitness-app/src/components/content/search/User.js
+++ fitness-app/src/components/content/search/User.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../../../styles/user.css'
 
 const User = ({user, setSingleUser ,setCurrentPage}) => {
   const handleClick = e => {
@@ -6,10 +7,22 @@ const User = ({user, setSingleUser ,setCurrentPage}) => {
     setCurrentPage('User Page')
   }
 
+  const handleDescription = () => user.description.length > 90 ? user.description.substring(0, 90) + "..." : user.description
+
   return(
-    <div>
-      <img src={user.profilepic}/>
-      <p onClick={handleClick} title={user.firstname}>{user.firstname} {user.lastname}</p>
+    
+    <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+      <div className="col p-4 d-flex flex-column position-static bg-light">
+        <h3 className="mb-0">{user.firstname} {user.lastname}</h3>
+        <div className="mb-1 text-muted">Region: {user.region}</div>
+        <p className="mb-auto profile-desc">{handleDescription()}</p>
+        <div className="justify-content-center">
+          <a onClick={handleClick} className="btn btn-dark text-light profile-link">Full profile</a>
+        </div>
+      </div>
+      <div className="col-auto d-none d-lg-block">
+        <img className="bd-placeholder-image" src={user.profilepic}/>
+      </div>
     </div>
   )
 }
