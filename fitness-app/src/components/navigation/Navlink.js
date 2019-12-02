3c0b33f3593e615e33c1fb8f764ce9abf32e164f
wesenbergg <b.b.j.1998@gmail.com>
2019-12-02 16:51:40 +0200

    qa disabled '-.-

--- fitness-app/src/components/navigation/Navlink.js
+++ fitness-app/src/components/navigation/Navlink.js
@@ -3,6 +3,15 @@ import React from 'react'
 const Navlink = ({name, setCurrentPage, styleAttr}) => {
   const handleNavClick = e => setCurrentPage(e.target.title)
 
+  if(name === 'QA'){
+    return(
+      <>
+        <li className="nav-item">
+            <a className="nav-link disabled" title={name} onClick={handleNavClick} tabIndex="-1" aria-disabled="true">{name}</a>
+        </li>
+      </>
+    )
+  }
   return(
     <>
       <li className="nav-item">
