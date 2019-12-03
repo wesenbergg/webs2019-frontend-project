2433d6ef6952bbb1d7ec26885a043036266c98c9
wwaltsu <walter.ruoppa@metropolia.fi>
2019-12-03 17:16:37 +0200

    Form uusyritys

--- fitness-app/src/components/content/about/EditProfile.js
+++ fitness-app/src/components/content/about/EditProfile.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../../../styles/EditProfile.css'
 import walter from "../../../img/walter.jpg"
+import a from "../../../img/test.png"
 const EditProfile = () => {
 const editButton = <button type="submit">Update profile</button>
 
@@ -9,26 +10,31 @@ const editButton = <button type="submit">Update profile</button>
             <div>
 
             <form>
-                <div className="d-flex justify-content-center">
-                <h3>Basic information </h3> {editButton}
-                </div>
-                    <p> username: </p>
-                    <p> real name:</p>
-                    <p> password:</p>
-                    <p> email:</p>
-                    <p> birth:</p>
 
+                <p> <h4> Walter Ruoppa </h4> </p>
+                <div className="align-self-end">
+                    <img  alt="Walter" src={walter}/>
+                </div>
+                <p> <h5> Walter95 </h5></p>
+                <p> <h5> Finland </h5> </p>
+                <h4>About me</h4>
+                Harrastuksiini kuuluu sali ja cardio. Harrastan välillä tanssia myös. Vapaa-aikani tykkään viettää Espanjan lämmössä, juoden sangriaa, olutta ja mojitoja.
                 <div className="d-flex justify-content-center">
-                <h3>Profile picture </h3>
                 </div>
                 <div>
-                <img className="kuvaEdit" alt="Walter" src={walter}/>
+
                 </div>
                 <div className="d-flex justify-content-center">
-                 <h3> Email adress </h3>
+                 <h3> Posts </h3>
                 </div>
-                <p> walsu@gmail.com</p>
-                <div>
+
+                <div className="block">
+
+                    <p><img className="image1"  alt="a" src={a}/></p>
+                    <p><img className="image1"  alt="a" src={a}/></p>
+                    <p><img className="image1"  alt="a" src={a}/></p>
+                    <p><img className="image1"  alt="a" src={a}/></p>
+                    <p><img className="image1"  alt="a" src={a}/></p>
 
                 </div>
             </form>
