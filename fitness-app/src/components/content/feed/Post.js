dabebb9b8260f94fc106839faa9309a9cb7e45d7
wwaltsu <walter.ruoppa@metropolia.fi>
2019-12-06 18:07:26 +0200

    userPost

--- fitness-app/src/components/content/feed/Post.js
+++ fitness-app/src/components/content/feed/Post.js
@@ -28,11 +28,11 @@ function handleClick({ setCurrentPage, setSinglePost, id }) {
 const Post = ({ title, text, author_id, image_url, date, setCurrentPage, setSinglePost, author_name, id }) => {
     if (image_url === null || image_url === undefined || image_url === "")
         return (
-            <div className="mx-auto post-thumb">
+            <div className="mx-auto post-thumb" align="center">
                 <div className="card mb-3" className="horizontalPost">
                     <div className="row no-gutters">
                         <div className="col-md-12">
-                            <div className="card-body">
+                            <div className="card-body fix">
                             <h4 className="card-title mouse-pointer" onClick={() => handleClick({ setCurrentPage, setSinglePost, id })}><Link to={`/posts/${id}`} >{title}</Link></h4>
                                 <p className="card-text">{text}</p>
                                 <p className="card-text"><small className="text-muted">From: <span className="card-link author-link mouse-pointer">{author_name}</span> {date}</small></p>
@@ -44,14 +44,14 @@ const Post = ({ title, text, author_id, image_url, date, setCurrentPage, setSing
         )
     if (image_url.includes('/assets/images')) {
         return (
-            <div className="mx-auto post-thumb">
+            <div className="mx-auto post-thumb" align="center">
                 <div className="card mb-3" className="horizontalPost">
                     <div className="row no-gutters">
                         <div className="col-md-4">
                             <img src={`${process.env.PUBLIC_URL}${image_url}`} className="card-img" alt="Image not found" />
                         </div>
-                        <div className="col-md-8">
-                            <div className="card-body">
+                        <div className="col-md-8 fix">
+                            <div className="card-body ">
                             <h4 className="card-title mouse-pointer" onClick={() => handleClick({ setCurrentPage, setSinglePost, id })}><Link to={`/posts/${id}`} >{title}</Link></h4>
                                 <p className="card-text">{text}</p>
                                 <p className="card-text"><small className="text-muted">From: <span className="card-link author-link mouse-pointer">{author_name}</span> {date}</small></p>
@@ -63,14 +63,14 @@ const Post = ({ title, text, author_id, image_url, date, setCurrentPage, setSing
         )
     } else {
         return (
-            <div className="mx-auto post-thumb">
+            <div className="mx-auto post-thumb" align="center">
                 <div className="card mb-3" className="horizontalPost">
                     <div className="row no-gutters">
                         <div className="col-md-4">
                             <img src={image_url} className="card-img" className="card-img" alt="No image found" />
                         </div>
                         <div className="col-md-8">
-                            <div className="card-body">
+                            <div className="card-body fix">
                                 <h4 className="card-title mouse-pointer" onClick={() => handleClick({ setCurrentPage, setSinglePost, id })}><Link to={`/posts/${id}`} >{title}</Link></h4>
                                 <p className="card-text">{text}</p>
                                 <p className="card-text"><small className="text-muted">From: <span className="card-link author-link mouse-pointer">{author_name}</span> {date}</small></p>
