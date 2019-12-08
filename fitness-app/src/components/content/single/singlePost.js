92cf95a574d5e5113ed971b28f098d8e347f1360
eliasker <eliasker@gmail.com>
2019-12-08 16:06:25 +0200

    single post alustava(!) layout

--- fitness-app/src/components/content/single/singlePost.js
+++ fitness-app/src/components/content/single/singlePost.js
@@ -1,22 +1,37 @@
 import React from 'react'
-import Post from '../feed/Post'
 import '../../../styles/post.css'
 
-const SinglePost = ({ singlePost, setCurrentPage }) => {
+// Tarkoitus olisi hakea käyttäjän profiilikuva users:sta
+const ProfilePic = () => {
     return (
-        <div className="singlePost">
-            <Post
-                key={singlePost.id}
-                title={singlePost.title}
-                text={singlePost.text}
-                author_id={singlePost.author_id}
-                image_url={singlePost.image}
-                date={singlePost.date}
-                id={singlePost.id}
-                author_name={singlePost.author_name}
-            />
-            <p>Likes tähän?(Postilta)</p>
-            <p>Kommentit </p>
+        <img className="singlePostAuthorInfo" src={`${process.env.PUBLIC_URL}/assets/images/default_avatar.png`} />
+    )
+}
+const SinglePostImage = ({ imageSrc }) => {
+    if (imageSrc === null || imageSrc === undefined || imageSrc === "") return null
+    return (
+        <img src={imageSrc} alt="Image not found" />
+    )
+}
+const SinglePost = ({ singlePost }) => {
+    console.log(singlePost.autho_name, singlePost.date)
+    return (
+        <div className="singlePostOuter">
+            <div className="singlePostInner">
+                <h3 className="singlePostTitle">{singlePost.title}</h3>
+                <section className="singlePostAuthorInfo">
+                    <div>
+                        <ProfilePic />
+                    </div>
+                    <div>
+                        <p><a href="">{singlePost.author_name}</a> {singlePost.date}</p>
+                    </div>
+                </section>
+                <div className="singlePostContent">
+                    <SinglePostImage imageSrc={singlePost.image} />
+                    <p className="singlePostContent">{singlePost.text}</p>
+                </div>
+            </div>
         </div>
     )
 }
